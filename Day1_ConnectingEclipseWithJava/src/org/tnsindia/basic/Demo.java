bf0e71722e8b9b204a7a0ddb88ff95b38bbd40a4
Rutvik_Sakpal <Rutvik_Sakpal@LAPTOP-CCS0N10J>
2022-09-29 11:24:44 +0530

    First Commit

---
+++ Day1_ConnectingEclipseWithJava/src/org/tnsindia/basic/Demo.java
@@ -0,0 +1,13 @@
+package org.tnsindia.basic;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=12;
+		System.out.println(x);
+		
+
+	}
+
+}
